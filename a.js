0501cf641d759075e48db2a6455386d38913b461
Daniel Taylor <dtaylo24@gmu.edu>
2017-08-15 11:15:13 -0400

    Hardcoded injection for reading vec3s is working

--- a.js
+++ a.js
@@ -17,7 +17,7 @@ void main() {
 
 var fragmentShaderSource = `#version 300 es
 
-precision mediump float;
+precision highp float;
 
 uniform vec2 iResolution;
 uniform float iTime;
@@ -32,14 +32,16 @@ uniform sampler2D iChannel2;
 // we need to declare an output for the fragment shader
 out vec4 outColor;
 
-/*void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-	vec2 uv = fragCoord.xy / iResolution.xy;
-	fragColor = vec4(uv,0.5+0.5*sin(iTime),1.0);
-}*/
+// for analyzer purposes
+int counter = 0;
+vec4 analyzer_output = vec4(0,0,0,1);
 
 CODE_HERE
 
+ENTRY_POINT_HERE
+`;
+
+var previewEntryPoint = `
 void main() {
 	vec2 fragCoord = gl_FragCoord.xy;
 
@@ -50,8 +52,18 @@ void main() {
 	{
 		outColor = mix(outColor, vec4(1,0,0,1), 0.5);
 	}
-}
-`;
+}`
+
+var varWatchEntryPoint = `
+void main() {
+	vec2 fragCoord = ANALYZER_MOUSE;
+
+	mainImage(outColor, fragCoord);
+
+	outColor = analyzer_output;
+}`
+
+var shadertoyRaw = ""
 
 function createShader(gl, type, source) {
 	var shader = gl.createShader(type);
@@ -82,22 +94,7 @@ function createProgram(gl, vertexShader, fragmentShader) {
 	return undefined;
 }
 
-var iChannel0Tex;
-
-var gl;
-var program;
-
-var vao;
-
-var iResolutionPos;
-var iTimePos;
-var iMousePos;
-var iChannel0Pos;
-
-var analyzerMousePos;
-var mousePos = { "x":0, "y":0 };
-
-function loadImage(url)
+function loadImage(gl, url)
 {
 	var tex = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_2D, tex);
@@ -108,7 +105,7 @@ function loadImage(url)
 		gl.bindTexture(gl.TEXTURE_2D, tex);
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
 		gl.generateMipmap(gl.TEXTURE_2D);
-		draw();
+		drawAll();
 	})
 	img.src = url;
 
@@ -118,79 +115,41 @@ function loadImage(url)
 function drawCode()
 {
 	var arr = fragmentShaderSource.split("\n");
+	//var s = "if(int(gl_FragCoord.x+0.5)==counter){analyzer_output.xyz=sorigin}counter+=1;"
+	//arr.splice(212, 0, s);
 	for (var i = 0; i < arr.length; ++i) {
 		//arr[i] = arr[i].replace(" ", "&nbsp");
 		//arr[i] = arr[i].replace("\t", "&nbsp &nbsp ");
-		$("#code").append("<tr>  <td><pre>" + i + "</pre></td> <td><pre>" + arr[i] + "</pre></td>  </tr>");
+		$("#code").append("<tr class='code-row-" + i + "'>  <td><pre>" + i + "</pre></td> <td><pre>" + arr[i] + "</pre></td>  </tr>");
 	}
 }
 
-function init() {
-	// Get A WebGL context
-	var canvas = document.getElementById("c");
-	gl = canvas.getContext("webgl2");
-	if (!gl) {
-		return;
-	}
+function setupResources(gl, fragSource)
+{
+	var result = {};
 
-	var done = false;
+	result.iChannel0Tex = loadImage(gl, "bricks.jpg");
 
-	$.ajax({url: "https://www.shadertoy.com/api/v1/shaders/Ml3Gzr?key=Nd8tWz", async:false, success: function(result) {
-		fragmentShaderSource = fragmentShaderSource.replace("CODE_HERE", result["Shader"]["renderpass"][0]["code"]);
-	}});
-
-	iChannel0Tex = loadImage("bricks.jpg");
-
-	canvas.width = canvas.clientWidth;
-	canvas.height = canvas.clientHeight;
-
-	// create GLSL shaders, upload the GLSL source, compile the shaders
 	var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-	var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-
-	// Link the two shaders into a program
-	program = createProgram(gl, vertexShader, fragmentShader);
+	var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragSource);
+	result.program = createProgram(gl, vertexShader, fragmentShader);
 
 	// look up where the vertex data needs to go.
-	var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+	var positionAttributeLocation = gl.getAttribLocation(result.program, "a_position");
 
-	iResolutionPos = gl.getUniformLocation(program, "iResolution");
-	iTimePos = gl.getUniformLocation(program, "iTime");
-	iMousePos = gl.getUniformLocation(program, "iMouse");
-	iChannel0Pos = gl.getUniformLocation(program, "iChannel0");
+	result.iResolutionPos = gl.getUniformLocation(result.program, "iResolution");
+	result.iTimePos = gl.getUniformLocation(result.program, "iTime");
+	result.iMousePos = gl.getUniformLocation(result.program, "iMouse");
+	result.iChannel0Pos = gl.getUniformLocation(result.program, "iChannel0");
 
-	analyzerMousePos = gl.getUniformLocation(program, "ANALYZER_MOUSE");
+	result.analyzerMousePos = gl.getUniformLocation(result.program, "ANALYZER_MOUSE");
 
-	/*var texture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, iChannel0Tex);
-	gl.generateMipmap(gl.TEXTURE_2D);*/
-	//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
-	// Create a buffer and put three 2d clip space points in it
 	var positionBuffer = gl.createBuffer();
-
-	// Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-	var positions = [
-		-1.0, -1.0,
-		 1.0,  1.0,
-		-1.0,  1.0,
-		-1.0, -1.0,
-		 1.0,  1.0,
-		 1.0, -1.0,
-	];
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-	// Create a vertex array object (attribute state)
-	vao = gl.createVertexArray();
-
-	// and make it the one we're currently working with
-	gl.bindVertexArray(vao);
-
-	// Turn on the attribute
+	result.vao = gl.createVertexArray();
+	gl.bindVertexArray(result.vao);
 	gl.enableVertexAttribArray(positionAttributeLocation);
 
 	// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
@@ -199,52 +158,173 @@ function init() {
 	var normalize = false; // don't normalize the data
 	var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
 	var offset = 0;        // start at the beginning of the buffer
-	gl.vertexAttribPointer(
-			positionAttributeLocation, size, type, normalize, stride, offset);
+	gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
 
-	//webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-	// Tell WebGL how to convert from clip space to pixels
 	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-	draw();
+	return result;
+}
+
+var positions = [
+	-1.0, -1.0,
+	1.0,  1.0,
+	-1.0,  1.0,
+	-1.0, -1.0,
+	1.0,  1.0,
+	1.0, -1.0,
+];
+
+var preview_gl;
+var preview_data;
+
+var varWatch_gl;
+var varWatch_data;
+
+var mousePos = { "x":0, "y":0 };
+
+function createTexture(gl, color) {
+	const tex = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, tex);
+	const level = 0;
+	const internalFormat = gl.RGBA32F;
+	const width = 1;
+	const height = 1;
+	const border = 0;
+	const format = gl.RGBA;
+	const type = gl.FLOAT;
+	const data = new Float32Array(color);
+	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border,
+				  format, type, data);
+	// unless we get `OES_texture_float_linear` we can not filter floating point
+	// textures
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	
+	return tex;
+  }
+
+function init() {
+
+	//var shader_url = "https://www.shadertoy.com/api/v1/shaders/Ml3Gzr?key=Nd8tWz";
+	var shader_url = "example.fs";
+	
+	$.ajax({url: shader_url, async:false, cache:false, success: function(result) {
+		//shadertoyRaw = result["Shader"]["renderpass"][0]["code"];
+		shadertoyRaw = result;
+		fragmentShaderSource = fragmentShaderSource.replace("CODE_HERE", shadertoyRaw);
+	}});
+
+	drawCode();
+
+	// PREVIEW GL SETUP
+	var preview_canvas = document.getElementById("preview");
+	preview_gl = preview_canvas.getContext("webgl2");
+	if (!preview_gl) {
+		console.log("asdfasdsfd");
+		return;
+	}
+
+	var previewFragSource = fragmentShaderSource.replace("ENTRY_POINT_HERE", previewEntryPoint);
+	preview_canvas.width = preview_canvas.clientWidth;
+	preview_canvas.height = preview_canvas.clientHeight;
+
+	preview_data = setupResources(preview_gl, previewFragSource);
+
+	// PREVIEW GL SETUP
+	var varWatch_canvas = document.getElementById("varWatch");
+	varWatch_gl = varWatch_canvas.getContext("webgl2");
+	if (!varWatch_gl) {
+		return;
+	}
+	const ext = varWatch_gl.getExtension("EXT_color_buffer_float");
+	if (!ext) {
+		alert("need EXT_color_buffer_float to work");
+		return;
+	}
+
+	var varWatchFragSource = fragmentShaderSource.replace("ENTRY_POINT_HERE", varWatchEntryPoint);
+	{
+		var arr = varWatchFragSource.split("\n");
+		//var s = "\t\tif(int(gl_FragCoord.x+0.5)==counter) { analyzer_output.xyz=sorigin; } \ncounter+=1;"
+		var s = "\t\tif(int(gl_FragCoord.x+0.1)==counter) { analyzer_output.xyz=v; } \ncounter+=1;"
+		arr.splice(27, 0, s);
+		varWatchFragSource = arr.join("\n");
+		console.log(varWatchFragSource);
+	}
+	/*var s = "if(int(gl_FragCoord.x+0.5)==counter){analyzer_output.xyz=sorigin}counter+=1;"
+	arr.splice(212, 0, s);*/
+	//var previewFragSource = fragmentShaderSource.replace("ENTRY_POINT_HERE", previewEntryPoint);
+	varWatch_canvas.width = varWatch_canvas.clientWidth;
+	varWatch_canvas.height = varWatch_canvas.clientHeight;
+
+	varWatch_data = setupResources(varWatch_gl, varWatchFragSource);
+
+	var fbTex = createTexture(varWatch_gl, [15, 19, 0, -7]);
+	var fb = varWatch_gl.createFramebuffer();
+	varWatch_gl.bindFramebuffer(varWatch_gl.FRAMEBUFFER, fb);
+	varWatch_gl.framebufferTexture2D(varWatch_gl.FRAMEBUFFER, varWatch_gl.COLOR_ATTACHMENT0, varWatch_gl.TEXTURE_2D, fbTex, 0);
+	varWatch_gl.drawBuffers([varWatch_gl.COLOR_ATTACHMENT0]);
+
+	drawAll();
 
 	$("#mouseX, #time").on('input', function() {
 		//console.log($("#time").val());
-		draw();
+		drawAll();
 	});
-	$("#c").click(function(e) {
+	$("#preview").click(function(e) {
 		var x = e.pageX - this.offsetLeft;
 		var y = e.pageY - this.offsetTop;
 		mousePos["x"] = x;
 		mousePos["y"] = y;
-		draw();
+		drawAll();
 	});
-
-	drawCode();
+	$("#analyzer_var").on('click', function(e) {
+		console.log("asdfasf");
+		//alert(window.getSelection());
+		analyze_selection();
+	});
 }
 
-function draw()
+function analyze_selection()
 {
-	// Clear the canvas
+	console.log($(window.getSelection().anchorNode).parents().eq(2).attr("class").split(' '));
+}
+
+function drawAll()
+{
+	if (!preview_gl || !preview_gl)
+		return;
+
+	draw(preview_gl, preview_data);
+	draw(varWatch_gl, varWatch_data);
+
+	{
+		varWatch_gl.readBuffer(varWatch_gl.COLOR_ATTACHMENT0);
+		
+		var pixels = new Float32Array(varWatch_gl.canvas.width * 4);
+		varWatch_gl.readPixels(0, 0, varWatch_gl.canvas.width, 1, varWatch_gl.RGBA, varWatch_gl.FLOAT, pixels);
+		console.log(pixels);
+	}
+}
+
+function draw(gl, data)
+{
 	gl.clearColor(0, 0, 0, 0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
-	// Tell it to use our program (pair of shaders)
-	gl.useProgram(program);
+	gl.useProgram(data.program);
 
-	// Bind the attribute/buffer set we want.
-	gl.bindVertexArray(vao);
+	gl.bindVertexArray(data.vao);
 
-	gl.uniform2f(iResolutionPos, gl.canvas.width, gl.canvas.height);
-	gl.uniform1f(iTimePos, $("#time").val());
-	gl.uniform4f(iMousePos, $("#mouseX").val(), 0., 0., 0.);
+	gl.uniform2f(data.iResolutionPos, gl.canvas.width, gl.canvas.height);
+	gl.uniform1f(data.iTimePos, $("#time").val());
+	gl.uniform4f(data.iMousePos, $("#mouseX").val(), 0., 0., 0.);
 
 	gl.activeTexture(gl.TEXTURE0 + 0);
-	//gl.bindTexture(gl.TEXTURE_2D, texture);
-	gl.uniform1i(iChannel0Pos, 0);
+	gl.bindTexture(gl.TEXTURE_2D, data.iChannel0Tex);
+	gl.uniform1i(data.iChannel0Pos, 0);
 
-	gl.uniform2f(analyzerMousePos, mousePos["x"], mousePos["y"]);
+	gl.uniform2f(data.analyzerMousePos, mousePos["x"], mousePos["y"]);
 
 	// draw
 	var primitiveType = gl.TRIANGLES;
@@ -253,4 +333,6 @@ function draw()
 	gl.drawArrays(primitiveType, offset, count);
 }
 
-init();
+$(document).ready(function() {
+	init();
+});
